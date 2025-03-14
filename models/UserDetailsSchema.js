967fe091e191b68b63eb92172159e801497ba999
Arjabi <arjabishrestha@gmail.com>
2025-03-14 21:28:04 +0545

    created schema for user details, user role and signup

---
+++ models/UserDetailsSchema.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    full_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: { 
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    },
+    phone_number: {
+        type: String,
+        required: true,
+        match: [/^98\d{8}$/, "Phone number must start with 98 and be 10 digits long"],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role", // Reference to Role collection
+        required: true,
+    },
+    registrationDate: {
+        type: Date,
+        default: Date.now,
+    },
+    verificationStatus: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+// Create the User model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
